f3b2a799fad219012a55806870309768bc221a41
Ratheshan03 <lionratheshan@gmail.com>
2021-08-13 17:00:51 +0530

    Bmi-calc done

---
+++ BMI-Calculator/script.js
@@ -0,0 +1,10 @@
+function BMI() {
+  let height = document.getElementById("h").value;
+  let weight = document.getElementById("w").value;
+  let bmi = weight / (((height / 100) * height) / 100);
+  let bmio = bmi.toFixed(2);
+
+  document.getElementById("result").innerHTML = "Your BMI is " + bmio;
+  document.getElementById("h").value = "";
+  document.getElementById("w").value = "";
+}
